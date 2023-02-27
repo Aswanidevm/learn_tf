c9305f5697207e2fd80bedf1704388a6d4c037c1
Aswanidevm <maswanidev@gmail.com>
2023-02-27 11:57:16 +0300

    user

--- roboshop/exa2/exam2.tf
+++ roboshop/exa2/exam2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  count                  = length{var.instances}
+  count                  = length(var.instances)
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-04f9cffce7d6ee23b"]
