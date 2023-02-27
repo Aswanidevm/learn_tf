cc758ccccb5a184dd95db527e8c4a8d3ee6c7066
Aswanidevm <maswanidev@gmail.com>
2023-02-27 11:56:13 +0300

    user

--- roboshop/exa2/exam2.tf
+++ roboshop/exa2/exam2.tf
@@ -1,9 +1,13 @@
 resource "aws_instance" "ec2" {
-  count                  = 3
+  count                  = length{var.instances}
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-04f9cffce7d6ee23b"]
   tags = {
-    Name = "test, test1, test2"
+    Name =var.instances [count.index]
   }
+}
+
+variable "insances" {
+        default = {"cart", "frontend","mysql"}
 }
