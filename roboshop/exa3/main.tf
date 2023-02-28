1f52e268b8378d1e70c60d2cbbed34647c24cc6f
Aswanidevm <maswanidev@gmail.com>
2023-02-28 07:35:46 +0300

    user

--- roboshop/exa3/main.tf
+++ roboshop/exa3/main.tf
@@ -1,5 +1,10 @@
 resource "aws_instance" "ec2" {
-
+  for_each = var.instances
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = each.value[var.instances "type"]
+  vpc_security_group_ids = ["sg-04f9cffce7d6ee23b"]
+  tags = {
+    Name =each.value[var.instances "Name"]
 }
 
 variable "instances"
@@ -10,11 +15,12 @@ variable "instances"
     {
       Name = "catalogue"
       type = "t3.micro"
-    } catalogue=
-  {
-    Name = "catalogue"
-    type = "t3.micro"
-  }
+    }
+    frontend=
+    {
+      Name = "frondend"
+      type = "t3.micro"
+    }
   }
 }
 
