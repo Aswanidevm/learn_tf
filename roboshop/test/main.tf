4f9734158e450139a820b5b8cb8d5e951dd11a8c
Aswanidevm <maswanidev@gmail.com>
2023-03-02 09:11:02 +0300

    user

--- roboshop/test/main.tf
+++ roboshop/test/main.tf
@@ -1,23 +1,24 @@
 module "ec2" {
-  for_each = var.instances
-  source = "./main"
-  component =each.value["Name"]
-  instance_type    = each.value["type"]
+  for_each        = var.instances
+  source          = "./main"
+  component       = each.value["Name"]
+  instance_type   = each.value["type"]
+  sg_id           = module.sg.sg_id
 }
 
 module "sg" {
-   source = "./sg"
+   source         = "./sg"
 }
 
 variable "instances"{
-  default =  {
-    catalogue=  {
-      Name = "catalogue"
-      type = "t3.micro"
+  default     =  {
+    catalogue =  {
+      Name    = "catalogue"
+      type    = "t3.micro"
     }
-    frontend=   {
-      Name = "frondend"
-      type = "t3.micro"
+    frontend  =   {
+      Name    = "frondend"
+      type    = "t3.micro"
     }
   }
 }
--- roboshop/test/main/main.tf
+++ roboshop/test/main/main.tf
@@ -1,25 +1,26 @@
 data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = ["973714476881"]
+  most_recent   = true
+  name_regex    = "Centos-8-DevOps-Practice"
+  owners        = ["973714476881"]
 }
 resource "aws_instance" "ec2"{
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = ["sg-04f9cffce7d6ee23b"]
+  vpc_security_group_ids = var.sg_id
   tags                   =  {
-    Name = var.component
+    Name                 = var.component
   }
 }
 
 resource "aws_route53_record" "record"{
-  zone_id = "Z04818282BOE8RVGV13K7"
-  name    = "${var.component}.myprojecdevops.info"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.ec2.private_ip]
+  zone_id     =   "Z04818282BOE8RVGV13K7"
+  name        =   "${var.component}.myprojecdevops.info"
+  type        =   "A"
+  ttl         =   30
+  records     =   [aws_instance.ec2.private_ip]
 }
 
 
 variable "component" {}
 variable "instance_type" {}
+variable "sg_id" {}
