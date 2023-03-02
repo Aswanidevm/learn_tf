e2a8d66f190b47147ac3cfa9f2070adcd245c4ef
Aswanidevm <maswanidev@gmail.com>
2023-03-02 11:10:35 +0300

    user

--- roboshop/test2/main.tf
+++ roboshop/test2/main.tf
@@ -14,7 +14,7 @@ module "sg" {
 module "route53" {
   for_each = var.instances
   source = "./route53"
-  priv_id = module.ec2.priv_id
+  priv_id = [module.ec2.priv_id]
   component = each.value["Name"]
   instance_type = each.value["type"]
   sg_id = module.sg.sg_id
